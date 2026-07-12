0b66579bd98243e6bba832a96af93b7c0990bc9b
Peter Kaizer <pdkaizer@hey.com>
2026-07-11 20:29:53 -0400

    Add currently-reading hero section to homepage
    Pulls from a small _data file so the featured book can be updated
    without touching the finished-books collection.

---
+++ src/_data/currentlyReading.js
@@ -0,0 +1,8 @@
+// Update this whenever you start a new book. Set to null to hide the hero.
+export default {
+  title: "The Keeper",
+  author: "Tana French",
+  isbn: "9780593493465",
+  coverUrl: "https://images4.penguinrandomhouse.com/cover/9780593493465",
+  excerpt: "On a cold night in a remote Irish village, a girl goes missing. Sweet, loving Rachel Holohan was about to be engaged to the son of the local big shot. Instead, she's dead in the river. The third and final novel in the Cal Hooper trilogy.",
+};
