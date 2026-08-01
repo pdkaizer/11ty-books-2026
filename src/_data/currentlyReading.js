ea03d9f0355cc4447b0e0c5cccbcb9c30ca0f39a
Peter Kaizer <pdkaizer@hey.com>
2026-07-31 20:44:33 -0400

    Update Ironwood reading progress to 23%

--- src/_data/currentlyReading.js
+++ src/_data/currentlyReading.js
@@ -5,5 +5,5 @@ export default {
   isbn: "9780316595384",
   coverUrl: "https://m.media-amazon.com/images/I/81j3kd54SdL._SL1500_.jpg",   // optional — cover image URL
   excerpt: "Acting on a tip from a confidential informant, Stilwell and his deputies watch a plane land in the middle of the night at the Airport in the Sky, a remote airstrip in the mountains. A duffel bag of drugs is dropped and the deputies move in, but things quickly go sideways.",
-  progress: 18,               // optional — % through the book
+  progress: 23,               // optional — % through the book
 };
